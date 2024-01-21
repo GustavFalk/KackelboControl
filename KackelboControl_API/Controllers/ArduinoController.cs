d7a6fd3f2a6e9e6abed429a2929b0f08c239ddf6
Gustav Falk <gustav.falk@paytrim.com>
2024-01-21 19:03:53 +0100

    added controller for calls from arduino

--- KackelboControl_API/Controllers/ArduinoController.cs
+++ KackelboControl_API/Controllers/ArduinoController.cs
@@ -1,4 +1,5 @@
 ﻿using KackelboControl_API.Authentication;
+using KackelboControl_API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KackelboControl_API.Controllers;
@@ -16,14 +17,41 @@ public class ArduinoController : ControllerBase
     }
 
     [HttpGet("sensorTriggers")]
-    public WeatherForecast GetAPA(string farbror)
+    public SensorTriggers GetSensorTriggers()
     {
-        return new WeatherForecast()
+        return new SensorTriggers()
         {
-            Date = DateOnly.FromDateTime(DateTime.Now),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            LightOn = new TimeOnly(08, 00),
+            LightOff = new TimeOnly(18, 00),
+            MinTemp = 10,
+            MaxTemp = 16,
+            UseSunLight = true,
+            SunUp = new TimeOnly(09, 00),
+            SunDown = new TimeOnly(16, 00)
         };
+    }
 
+    [HttpPost("sensorValues")]
+    public async Task<IActionResult> PostSensorValues(string innerTemp, string outerTemp, int hour, int minute)
+    {
+        TimeOnly arduinoTime = new TimeOnly(hour, minute);
+        await Task.Delay(1);
+        return StatusCode(200);
+    }
+
+    [HttpPost("light")]
+    public async Task<IActionResult> PostLightOn(bool lightOn, int hour, int minute)
+    {
+        TimeOnly arduinoTime = new TimeOnly(hour, minute);
+        await Task.Delay(1);
+        return StatusCode(200);
+    }
+
+    [HttpPost("heat")]
+    public async Task<IActionResult> PostHeatOn(bool heatOn, int hour, int minute)
+    {
+        TimeOnly arduinoTime = new TimeOnly(hour, minute);
+        await Task.Delay(1);
+        return StatusCode(200);
     }
 }
