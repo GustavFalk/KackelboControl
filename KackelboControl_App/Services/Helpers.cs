cc7302e4bcb8399fdef2d2fac73fb7a1d1da151a
Gustav Falk <gustav.falk@paytrim.com>
2024-02-09 23:51:07 +0100

    mady gui pretty

---
+++ KackelboControl_App/Services/Helpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KackelboControl_App.Services;
+
+public static class Helpers
+{
+
+    public static string DayToSwe(string day)
+    {
+        switch (day)
+        {
+            case "Monday":
+                return "MÅN";
+            case "Tuesday":
+                return "TIS";
+            case "Wednesday":
+                return "ONS";
+            case "Thursday":
+                return "TOR";
+            case "Friday":
+                return "FRE";
+            case "Saturday":
+                return "LÖR";
+            case "Sunday":
+                return "SÖN";
+            default:
+                return "";
+        }
+    }
+}
